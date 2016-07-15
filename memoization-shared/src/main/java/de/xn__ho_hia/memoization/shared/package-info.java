d9636bbdd361220377e8a7b8cc9bf0743f8b952d
Sebastian Hoß <mail@shoss.de>
2016-07-15 20:28:44 +0200

    fix #50
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- memoization-shared/src/main/java/de/xn__ho_hia/memoization/shared/package-info.java
+++ memoization-shared/src/main/java/de/xn__ho_hia/memoization/shared/package-info.java
@@ -1,1 +1,7 @@
+/*
+ * This file is part of memoization.java. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://creativecommons.org/publicdomain/zero/1.0/. No part of memoization.java,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms contained
+ * in the LICENSE file.
+ */
 package de.xn__ho_hia.memoization.shared;
--- memoization-shared/src/test/java/de/xn__ho_hia/memoization/shared/MemoizationExceptionTest.java
+++ memoization-shared/src/test/java/de/xn__ho_hia/memoization/shared/MemoizationExceptionTest.java
@@ -1,25 +1,13 @@
 /*
- * ysura GmbH ("COMPANY") CONFIDENTIAL
- * Unpublished Copyright (c) 2012-2015 ysura GmbH, All Rights Reserved.
- *
- * NOTICE:  All information contained herein is, and remains the property of COMPANY. The intellectual and technical concepts contained
- * herein are proprietary to COMPANY and may be covered by U.S. and Foreign Patents, patents in process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained
- * from COMPANY.  Access to the source code contained herein is hereby forbidden to anyone except current COMPANY employees, managers or contractors who have executed
- * Confidentiality and Non-disclosure agreements explicitly covering such access.
- *
- * The copyright notice above does not evidence any actual or intended publication or disclosure  of  this source code, which includes
- * information that is confidential and/or proprietary, and is a trade secret, of COMPANY. ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC PERFORMANCE,
- * OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS SOURCE CODE WITHOUT THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE
- * LAWS AND INTERNATIONAL TREATIES. THE RECEIPT OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS
- * TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
+ * This file is part of memoization.java. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://creativecommons.org/publicdomain/zero/1.0/. No part of memoization.java,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms contained
+ * in the LICENSE file.
  */
 package de.xn__ho_hia.memoization.shared;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-import de.xn__ho_hia.memoization.shared.MemoizationException;
 
 /**
  *
