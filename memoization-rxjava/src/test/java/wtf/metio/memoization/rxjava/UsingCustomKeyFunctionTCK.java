3348221372171ab4673b2d4a630baeb24dbc6499
Sebastian Hoß <seb@ho.de>
2022-12-10 12:13:18 +0100

    add missing tests for RxJava

--- memoization-rxjava/src/test/java/wtf/metio/memoization/rxjava/UsingCustomKeyFunctionTCK.java
+++ memoization-rxjava/src/test/java/wtf/metio/memoization/rxjava/UsingCustomKeyFunctionTCK.java
@@ -25,6 +25,60 @@ abstract class UsingCustomKeyFunctionTCK {
     }
 
     @Test
+    final void shouldMemoizeActionWithKeyFunction() throws Throwable {
+        // given
+        final Action action = Mockito.mock(Action.class);
+        final Supplier<String> keySupplier = Mockito.mock(Supplier.class);
+        Mockito.when(keySupplier.get()).thenReturn("key");
+
+        // when
+        final Action memoize = action(action, keySupplier);
+
+        // then
+        Assertions.assertNotNull(memoize);
+        threadedRun(DEFAULT_RUNS, () -> {
+            try {
+                memoize.run();
+            } catch (Throwable e) {
+                waiter.fail(e);
+            }
+            waiter.resume();
+        });
+        waiter.await(DEFAULT_WAIT, DEFAULT_RUNS);
+        Mockito.verify(action).run();
+        Mockito.verify(keySupplier, Mockito.times(DEFAULT_RUNS)).get();
+    }
+
+    protected abstract <KEY> Action action(Action action, Supplier<KEY> keySupplier);
+
+    @Test
+    final void shouldMemoizeCancellableWithKeyFunction() throws Throwable {
+        // given
+        final Cancellable cancellable = Mockito.mock(Cancellable.class);
+        final Supplier<String> keySupplier = Mockito.mock(Supplier.class);
+        Mockito.when(keySupplier.get()).thenReturn("key");
+
+        // when
+        final Cancellable memoize = cancellable(cancellable, keySupplier);
+
+        // then
+        Assertions.assertNotNull(memoize);
+        threadedRun(DEFAULT_RUNS, () -> {
+            try {
+                memoize.cancel();
+            } catch (Throwable e) {
+                waiter.fail(e);
+            }
+            waiter.resume();
+        });
+        waiter.await(DEFAULT_WAIT, DEFAULT_RUNS);
+        Mockito.verify(cancellable).cancel();
+        Mockito.verify(keySupplier, Mockito.times(DEFAULT_RUNS)).get();
+    }
+
+    protected abstract <KEY> Cancellable cancellable(Cancellable cancellable, Supplier<KEY> keySupplier);
+
+    @Test
     final void shouldMemoizeFunctionWithKeyFunction() throws Throwable {
         // given
         final Function<String, String> function = Mockito.mock(Function.class);
@@ -53,6 +107,216 @@ abstract class UsingCustomKeyFunctionTCK {
     protected abstract <INPUT, KEY, OUTPUT> Function<INPUT, OUTPUT> function(
             Function<INPUT, OUTPUT> function,
             Function<INPUT, KEY> keyFunction);
+
+    @Test
+    final void shouldMemoizeFunction3WithKeyFunction() throws Throwable {
+        // given
+        final Function3<String, String, String, String> function = Mockito.mock(Function3.class);
+        Mockito.when(function.apply("1", "2", "3")).thenReturn("value");
+        final Function3<String, String, String, String> keyFunction = Mockito.mock(Function3.class);
+        Mockito.when(keyFunction.apply("1", "2", "3")).thenReturn("key");
+
+        // when
+        final Function3<String, String, String, String> memoize = function3(function, keyFunction);
+
+        // then
+        Assertions.assertNotNull(memoize);
+        threadedRun(DEFAULT_RUNS, () -> {
+            try {
+                memoize.apply("1", "2", "3");
+            } catch (Throwable e) {
+                waiter.fail(e);
+            }
+            waiter.resume();
+        });
+        waiter.await(DEFAULT_WAIT, DEFAULT_RUNS);
+        Mockito.verify(function).apply("1", "2", "3");
+        Mockito.verify(keyFunction, Mockito.times(DEFAULT_RUNS)).apply("1", "2", "3");
+    }
+
+    protected abstract <TYPE1, TYPE2, TYPE3, KEY, OUTPUT> Function3<TYPE1, TYPE2, TYPE3, OUTPUT> function3(
+            Function3<TYPE1, TYPE2, TYPE3, OUTPUT> function,
+            Function3<TYPE1, TYPE2, TYPE3, KEY> keyFunction);
+
+    @Test
+    final void shouldMemoizeFunction4WithKeyFunction() throws Throwable {
+        // given
+        final Function4<String, String, String, String, String> function = Mockito.mock(Function4.class);
+        Mockito.when(function.apply("1", "2", "3", "4")).thenReturn("value");
+        final Function4<String, String, String, String, String> keyFunction = Mockito.mock(Function4.class);
+        Mockito.when(keyFunction.apply("1", "2", "3", "4")).thenReturn("key");
+
+        // when
+        final Function4<String, String, String, String, String> memoize = function4(function, keyFunction);
+
+        // then
+        Assertions.assertNotNull(memoize);
+        threadedRun(DEFAULT_RUNS, () -> {
+            try {
+                memoize.apply("1", "2", "3", "4");
+            } catch (Throwable e) {
+                waiter.fail(e);
+            }
+            waiter.resume();
+        });
+        waiter.await(DEFAULT_WAIT, DEFAULT_RUNS);
+        Mockito.verify(function).apply("1", "2", "3", "4");
+        Mockito.verify(keyFunction, Mockito.times(DEFAULT_RUNS)).apply("1", "2", "3", "4");
+    }
+
+    protected abstract <TYPE1, TYPE2, TYPE3, TYPE4, KEY, OUTPUT> Function4<TYPE1, TYPE2, TYPE3, TYPE4, OUTPUT> function4(
+            Function4<TYPE1, TYPE2, TYPE3, TYPE4, OUTPUT> function,
+            Function4<TYPE1, TYPE2, TYPE3, TYPE4, KEY> keyFunction);
+
+    @Test
+    final void shouldMemoizeFunction5WithKeyFunction() throws Throwable {
+        // given
+        final Function5<String, String, String, String, String, String> function = Mockito.mock(Function5.class);
+        Mockito.when(function.apply("1", "2", "3", "4", "5")).thenReturn("value");
+        final Function5<String, String, String, String, String, String> keyFunction = Mockito.mock(Function5.class);
+        Mockito.when(keyFunction.apply("1", "2", "3", "4", "5")).thenReturn("key");
+
+        // when
+        final Function5<String, String, String, String, String, String> memoize = function5(function, keyFunction);
+
+        // then
+        Assertions.assertNotNull(memoize);
+        threadedRun(DEFAULT_RUNS, () -> {
+            try {
+                memoize.apply("1", "2", "3", "4", "5");
+            } catch (Throwable e) {
+                waiter.fail(e);
+            }
+            waiter.resume();
+        });
+        waiter.await(DEFAULT_WAIT, DEFAULT_RUNS);
+        Mockito.verify(function).apply("1", "2", "3", "4", "5");
+        Mockito.verify(keyFunction, Mockito.times(DEFAULT_RUNS)).apply("1", "2", "3", "4", "5");
+    }
+
+    protected abstract <TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, KEY, OUTPUT> Function5<TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, OUTPUT> function5(
+            Function5<TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, OUTPUT> function,
+            Function5<TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, KEY> keyFunction);
+
+    @Test
+    final void shouldMemoizeFunction6WithKeyFunction() throws Throwable {
+        // given
+        final Function6<String, String, String, String, String, String, String> function = Mockito.mock(Function6.class);
+        Mockito.when(function.apply("1", "2", "3", "4", "5", "6")).thenReturn("value");
+        final Function6<String, String, String, String, String, String, String> keyFunction = Mockito.mock(Function6.class);
+        Mockito.when(keyFunction.apply("1", "2", "3", "4", "5", "6")).thenReturn("key");
+
+        // when
+        final Function6<String, String, String, String, String, String, String> memoize = function6(function, keyFunction);
+
+        // then
+        Assertions.assertNotNull(memoize);
+        threadedRun(DEFAULT_RUNS, () -> {
+            try {
+                memoize.apply("1", "2", "3", "4", "5", "6");
+            } catch (Throwable e) {
+                waiter.fail(e);
+            }
+            waiter.resume();
+        });
+        waiter.await(DEFAULT_WAIT, DEFAULT_RUNS);
+        Mockito.verify(function).apply("1", "2", "3", "4", "5", "6");
+        Mockito.verify(keyFunction, Mockito.times(DEFAULT_RUNS)).apply("1", "2", "3", "4", "5", "6");
+    }
+
+    protected abstract <TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, TYPE6, KEY, OUTPUT> Function6<TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, TYPE6, OUTPUT> function6(
+            Function6<TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, TYPE6, OUTPUT> function,
+            Function6<TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, TYPE6, KEY> keyFunction);
+
+    @Test
+    final void shouldMemoizeFunction7WithKeyFunction() throws Throwable {
+        // given
+        final Function7<String, String, String, String, String, String, String, String> function = Mockito.mock(Function7.class);
+        Mockito.when(function.apply("1", "2", "3", "4", "5", "6", "7")).thenReturn("value");
+        final Function7<String, String, String, String, String, String, String, String> keyFunction = Mockito.mock(Function7.class);
+        Mockito.when(keyFunction.apply("1", "2", "3", "4", "5", "6", "7")).thenReturn("key");
+
+        // when
+        final Function7<String, String, String, String, String, String, String, String> memoize = function7(function, keyFunction);
+
+        // then
+        Assertions.assertNotNull(memoize);
+        threadedRun(DEFAULT_RUNS, () -> {
+            try {
+                memoize.apply("1", "2", "3", "4", "5", "6", "7");
+            } catch (Throwable e) {
+                waiter.fail(e);
+            }
+            waiter.resume();
+        });
+        waiter.await(DEFAULT_WAIT, DEFAULT_RUNS);
+        Mockito.verify(function).apply("1", "2", "3", "4", "5", "6", "7");
+        Mockito.verify(keyFunction, Mockito.times(DEFAULT_RUNS)).apply("1", "2", "3", "4", "5", "6", "7");
+    }
+
+    protected abstract <TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, TYPE6, TYPE7, KEY, OUTPUT> Function7<TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, TYPE6, TYPE7, OUTPUT> function7(
+            Function7<TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, TYPE6, TYPE7, OUTPUT> function,
+            Function7<TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, TYPE6, TYPE7, KEY> keyFunction);
+
+    @Test
+    final void shouldMemoizeFunction8WithKeyFunction() throws Throwable {
+        // given
+        final Function8<String, String, String, String, String, String, String, String, String> function = Mockito.mock(Function8.class);
+        Mockito.when(function.apply("1", "2", "3", "4", "5", "6", "7", "8")).thenReturn("value");
+        final Function8<String, String, String, String, String, String, String, String, String> keyFunction = Mockito.mock(Function8.class);
+        Mockito.when(keyFunction.apply("1", "2", "3", "4", "5", "6", "7", "8")).thenReturn("key");
+
+        // when
+        final Function8<String, String, String, String, String, String, String, String, String> memoize = function8(function, keyFunction);
+
+        // then
+        Assertions.assertNotNull(memoize);
+        threadedRun(DEFAULT_RUNS, () -> {
+            try {
+                memoize.apply("1", "2", "3", "4", "5", "6", "7", "8");
+            } catch (Throwable e) {
+                waiter.fail(e);
+            }
+            waiter.resume();
+        });
+        waiter.await(DEFAULT_WAIT, DEFAULT_RUNS);
+        Mockito.verify(function).apply("1", "2", "3", "4", "5", "6", "7", "8");
+        Mockito.verify(keyFunction, Mockito.times(DEFAULT_RUNS)).apply("1", "2", "3", "4", "5", "6", "7", "8");
+    }
+
+    protected abstract <TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, TYPE6, TYPE7, TYPE8, KEY, OUTPUT> Function8<TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, TYPE6, TYPE7, TYPE8, OUTPUT> function8(
+            Function8<TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, TYPE6, TYPE7, TYPE8, OUTPUT> function,
+            Function8<TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, TYPE6, TYPE7, TYPE8, KEY> keyFunction);
+
+    @Test
+    final void shouldMemoizeFunction9WithKeyFunction() throws Throwable {
+        // given
+        final Function9<String, String, String, String, String, String, String, String, String, String> function = Mockito.mock(Function9.class);
+        Mockito.when(function.apply("1", "2", "3", "4", "5", "6", "7", "8", "9")).thenReturn("value");
+        final Function9<String, String, String, String, String, String, String, String, String, String> keyFunction = Mockito.mock(Function9.class);
+        Mockito.when(keyFunction.apply("1", "2", "3", "4", "5", "6", "7", "8", "9")).thenReturn("key");
+
+        // when
+        final Function9<String, String, String, String, String, String, String, String, String, String> memoize = function9(function, keyFunction);
+
+        // then
+        Assertions.assertNotNull(memoize);
+        threadedRun(DEFAULT_RUNS, () -> {
+            try {
+                memoize.apply("1", "2", "3", "4", "5", "6", "7", "8", "9");
+            } catch (Throwable e) {
+                waiter.fail(e);
+            }
+            waiter.resume();
+        });
+        waiter.await(DEFAULT_WAIT, DEFAULT_RUNS);
+        Mockito.verify(function).apply("1", "2", "3", "4", "5", "6", "7", "8", "9");
+        Mockito.verify(keyFunction, Mockito.times(DEFAULT_RUNS)).apply("1", "2", "3", "4", "5", "6", "7", "8", "9");
+    }
+
+    protected abstract <TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, TYPE6, TYPE7, TYPE8, TYPE9, KEY, OUTPUT> Function9<TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, TYPE6, TYPE7, TYPE8, TYPE9, OUTPUT> function9(
+            Function9<TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, TYPE6, TYPE7, TYPE8, TYPE9, OUTPUT> function,
+            Function9<TYPE1, TYPE2, TYPE3, TYPE4, TYPE5, TYPE6, TYPE7, TYPE8, TYPE9, KEY> keyFunction);
 
     @Test
     final void shouldMemoizeIntFunctionWithKeyFunction() throws Throwable {
